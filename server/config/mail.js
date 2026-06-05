57d77f10ea7a690ed76aff926c7f462954147412
sophia310 <sophiapattekari02@gmail.com>
2026-06-05 14:57:38 +0530

    switched to brevo

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,21 +1,12 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
-  secure: false, // true only for 465
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("MAIL ERROR:");
-    console.log(error);
-  } else {
-    console.log("MAIL SERVER READY");
+    user: process.env.BREVO_LOGIN,
+    pass: process.env.BREVO_PASSWORD
   }
 });
 
