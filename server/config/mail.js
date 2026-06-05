8fc5e89904a1ab4f4b78737ea01618f1f232322f
sophia310 <sophiapattekari02@gmail.com>
2026-06-05 13:32:49 +0530

    fixed nodemailer config

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
-  service: "gmail",
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true only for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
@@ -11,14 +11,10 @@ const transporter = nodemailer.createTransport({
 });
 
 transporter.verify((error, success) => {
-
   if (error) {
-
     console.log("MAIL ERROR:");
     console.log(error);
-
   } else {
-
     console.log("MAIL SERVER READY");
   }
 });
