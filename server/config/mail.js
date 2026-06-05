7e21900dc91844f23c97010673e9dbe9a8dcab65
sophia310 <sophiapattekari02@gmail.com>
2026-06-05 15:05:29 +0530

    brevo fix 2

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,5 +1,8 @@
 const nodemailer = require("nodemailer");
 
+console.log("SMTP USER:", process.env.BREVO_LOGIN);
+console.log("SMTP PASS EXISTS:", !!process.env.BREVO_PASSWORD);
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
